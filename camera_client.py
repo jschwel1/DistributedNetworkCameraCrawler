5fc07126ea64546cfea641945ec6435ef16b7ebe
Your Name <jschwel1@github.com>
2019-02-07 23:21:48 -0500

    Updated communication between client and server

--- camera_client.py
+++ camera_client.py
@@ -2,24 +2,34 @@
 import socket
 import threading
 import sys
+import pickle
 
 class CameraClient():
     
-    def __init__(self):
+    def __init__(self, name, left, right):
         self.ip = ''
         self.port = 0
         self.server_ip = ''
         self.server_port = int(sys.argv[1])
         self.socket = socket.socket()
+        self.name = name
+        self.left = left
+        self.right = right
         
         self.open_connection()
 
     def socket_thread(self):
-        self.socket.send(bytes('Dave', 'utf-8'))
+        info = {
+            'name': self.name,
+            'left': self.left,
+            'right': self.right,
+        }
+        self.socket.send(pickle.dumps(info))
+        print(str(self.socket.recv(2048), 'utf-8'))
         while True:
-            msg = str(self.socket.recv(1024), 'utf-8')
+            msg = pickle.loads(self.socket.recv(2048))
             if (msg != ''):
-                print('%s'%msg)
+                print(msg)
         self.socket.close()
 
     def open_connection(self):
@@ -30,10 +40,27 @@ class CameraClient():
 
 
 if __name__=='__main__':
-    #cc = CameraClient()
+    names = {
+        'N':{
+            'l':['W'],
+            'r':['E'],
+        },
+        'E':{
+            'l':['N'],
+            'r':['S'],
+        },
+        'S':{
+            'l':['E'],
+            'r':['W'],
+        },
+        'W':{
+            'l':['W'],
+            'r':['N'],
+        },
+    }
     cl = []
-    for i in range(125):
+    for i in names:
         print('%s )'%i)
-        cl.append(CameraClient())
+        cl.append(CameraClient(i, names[i]['l'], names[i]['r']))
 
     
--- camera_server.py
+++ camera_server.py
@@ -2,6 +2,7 @@
 
 import socket
 import threading
+import pickle
 
 class CameraServer():
     
@@ -10,6 +11,8 @@ class CameraServer():
         self.server_socket = socket.socket()
         self.max_connections = 100
         self.cameras = {}
+        self.alert_list = [] # list of tuples of alerts: [(who, from, side), (who, from, side), ...]
+        self.alert_list_lock = threading.Lock()
 
         # bind to port
         self.server_socket.bind(('', self.listen_port))
@@ -19,11 +22,31 @@ class CameraServer():
         self.await_connections()
 
     def new_connection(self, client):
-        msg = str(client.recv(64), 'utf-8')
-        print('Connected to %s'%msg)
-        self.cameras[msg] = client
-        #while True:
-        client.send(bytes('Welcome to the network, %s'%msg, 'utf-8'))
+        data = pickle.loads(client.recv(2048))
+        name = data['name']
+        left = data['left']
+        right = data['right']
+        
+        print('Connected to %s'%name)
+        self.cameras[name] = client
+        client.send(bytes('Welcome to the network, %s'%name, 'utf-8'))
+        while True:
+            # Acquire a lock on the alert list
+            self.alert_list_lock.acquire()
+            # See if this client is in the list of cameras to be alerted
+            if name in list(zip(*self.alert_list))[0]:
+                # get index of camera in the list
+                idx = list(zip(*self.alert_list))[0].index(name)
+                data = {
+                    'from': self.alert_list[idx][1],
+                    'side': self.alert_list[idx][2],
+                }
+                # Package the data and send it
+                client.send(pickle.dumps(data))
+                # Remove this alert from the list
+                del self.alert_list[idx]
+            # Release the mutex
+            self.alert_list_lock.release()
         client.close()
 
 
@@ -37,6 +60,6 @@ class CameraServer():
 
  
 
-
-cs = CameraServer()
+if __name__ == '__main__':
+    cs = CameraServer()
 
